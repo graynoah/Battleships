e9581f4c240f4e11a9185dc48dbd101ecd0bc4fa
ANinja_Cow <jumf2222@gmail.com>
2019-10-27 17:52:35 -0400

    Add a constants class
    This commit adds a constants class. This constants class is useful for
    defining constants to be use throughout the application.

---
+++ util/constants.py
@@ -0,0 +1,60 @@
+import pygame
+
+# Valid characters for user input
+VALID_CHARACTERS = ("abcdefghijklmnopqrstuvwxyz"
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    "1234567890!()[]_ ")
+
+# Cursor to be displayed when over a textbox
+TEXTBOX_CURSOR = ((8, 16), (4, 4), *pygame.cursors.compile((
+    "XXX XXX ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "   X    ",
+    "XXX XXX ")))
+
+# Cursor to be displayed when over a button
+BUTTON_CURSOR = ((24, 32), (10, 0), *pygame.cursors.compile((
+    "         XX             ",
+    "        X..X            ",
+    "        X..X            ",
+    "        X..X            ",
+    "        X..X            ",
+    "        X..X            ",
+    "        X..X            ",
+    "        X..XXX          ",
+    "        X..X..XXXX      ",
+    "        X..X..X..X      ",
+    "        X..X..X..XX     ",
+    "        X..X..X..X.X    ",
+    "   XXXX X..X..X..X..X   ",
+    "   X...XX...........X   ",
+    "   X....X...........X   ",
+    "    X...X...........X   ",
+    "     X..............X   ",
+    "     X..............X   ",
+    "     X..............X   ",
+    "      X.............X   ",
+    "      X............X    ",
+    "       X...........X    ",
+    "       X...........X    ",
+    "        X.........X     ",
+    "        X.........X     ",
+    "        XXXXXXXXXXX     ",
+    "                        ",
+    "                        ",
+    "                        ",
+    "                        ",
+    "                        ",
+    "                        ",)))
