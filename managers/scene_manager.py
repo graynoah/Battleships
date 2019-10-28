48a6c4b7fdbe89e35a16aaafd21b3ecde63e4d7a
ANinja_Cow <jumf2222@gmail.com>
2019-10-27 22:04:53 -0400

    Add multi-resolution support
    This commit adds features that supports multi-resolution including
    dynamic window resizing and fullscreen mode.

--- managers/scene_manager.py
+++ managers/scene_manager.py
@@ -1,4 +1,5 @@
 from typing import Tuple, List
+import os
 
 import pygame
 
@@ -7,6 +8,7 @@ from components.panel import Panel
 from components.style import Style
 from components.label import Label
 from components.component import Component
+from util.constants import DEFAULT_WINDOWED_MODE_SIZE
 
 from scene.game_scene import GameScene
 from scene.main_menu_scene import MainMenuScene
@@ -33,6 +35,12 @@ class SceneManager(object):
             A list of all available scenes.
         _running:
             Whether or not the application is currently running.
+        _is_fullscreen:
+            Whether or not the application is currently in fullscreen mode.
+        _active_scene_index:
+            The currently active scene's index.
+        _active_scene:
+            The currently active scene.
     """
     instance = None
 
@@ -42,36 +50,91 @@ class SceneManager(object):
     _fps_counter: Component
     _scenes: List
     _running: bool
+    _is_fullscreen: bool
+    _active_scene_index: int
+    _active_scene: object
 
     def __init__(self) -> None:
         """Create a new SceneManager and setup the static instance variable."""
         if(SceneManager.instance is None):
             SceneManager.instance = self
 
-        pygame.init()
-        # Open a new window
-        self._screen_size = (960, 540)
-        self._screen = pygame.display.set_mode(self._screen_size)
-        # , (pygame.FULLSCREEN | pygame.HWSURFACE)
-        pygame.display.set_caption("Battleships")
-        pygame.key.set_repeat(250, 50)
-        pygame.scrap.init()
+        # Tell pygame to center the window
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+        pygame.font.init()
 
-        # The clock will be used to control how fast the screen updates
+        self._screen_size = (0, 0)
+        self._is_fullscreen = True
         self._clock = pygame.time.Clock()
+        self._scenes = [MainMenuScene, SettingsScene]
+        self._running = True
 
-        self._root = Panel(pygame.Rect(0, 0, 960, 540), style=Style(
+        self._setup_screen()
+
+        # Always visible components
+        self._root = Panel(self._screen.get_rect(), style=Style(
             background_color=(255, 0, 0), border_width=0))
 
         em.EventManager()
 
-        self._fps_counter = Label(text="Calculating", rect=pygame.Rect(
-            960-200, 0, 200, 100), parent=self._root)
+        self._fps_counter = Label(text="Calculating",
+                                  rect=pygame.Rect(0, 25, 75, 20),
+                                  style=Style(
+                                      background_color=(255, 0, 0),
+                                      border_width=0,
+                                      primary_color=(255, 255, 255)),
+                                  parent=self._root)
+        self._rest_fps_counter_position()
 
-        self._scenes = [MainMenuScene, SettingsScene]
+        # Start the game
         self.change_scene(0)
-        self._running = True
         self._run_game_loop()
+
+    def set_screen_size(self, size: Tuple[int, int]) -> None:
+        """Set the screen size. This method re-creates the active scene."""
+        em.EventManager.instance.set_invalid()
+        self._screen_size = size
+        self._setup_screen()
+        self._root.set_rect(self._screen.get_rect())
+        self._rest_fps_counter_position()
+        self.change_scene(self._active_scene_index)
+
+    def _setup_screen(self) -> None:
+        """Create the screen object."""
+        pygame.display.init()
+        pygame.display.set_caption("Battleships")
+        pygame.key.set_repeat(250, 50)
+
+        flags = pygame.RESIZABLE
+        if(self._is_fullscreen):
+            flags |= pygame.FULLSCREEN
+
+        self._screen = pygame.display.set_mode(self._screen_size, flags)
+        self._screen_size = self._screen.get_rect().size
+
+    def is_fullscreen(self) -> bool:
+        """Whether or not the the application is in fullscreen mode."""
+        return self._is_fullscreen
+
+    def toggle_fullscreen(self) -> None:
+        """Toggles the application mode between fullscreen and windowed."""
+        self._is_fullscreen = not self._is_fullscreen
+        pygame.display.quit()
+
+        if(self._is_fullscreen):
+            self.set_screen_size((0, 0))
+        else:
+            self.set_screen_size(DEFAULT_WINDOWED_MODE_SIZE)
+
+    def _rest_fps_counter_position(self):
+        """Sets the position of the fps counter."""
+        fps_rect = self._fps_counter.get_rect()
+        fps_rect.right = self._screen_size[0] - 25
+        self._fps_counter.set_rect(fps_rect)
+
+    def get_screen_size(self) -> Tuple[int, int]:
+        """Get the size of the screen in pixels."""
+        return self._screen_size
 
     def get_root(self) -> Component:
         """Get the topmost component."""
@@ -80,6 +143,7 @@ class SceneManager(object):
     def change_scene(self, scene_index: int) -> None:
         """Switch the current scene to the scene at index <scene_index>."""
         self._root.clear_children()
+        self._active_scene_index = scene_index
         self._active_scene = self._scenes[scene_index](self._root)
         self._root.add_child(self._fps_counter)
         em.EventManager.instance.set_invalid()
