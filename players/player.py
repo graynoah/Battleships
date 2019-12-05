4ff1e62f468b4a8363ac0b42643877dcbbb1a6a9
Noah Gray <noah.gray@mail.utoronto.ca>
2019-12-04 19:20:22 -0500

    Add Docstrings to Player Class
    This commit adds: class, variable, and function descriptions,
    to the Player Class.  For clarity and readers convenience.

--- players/player.py
+++ players/player.py
@@ -1,44 +1,94 @@
-from typing import List
-from ships.ship import Ship
+from typing import List, Tuple
+
+from players.ship import Ship
+import managers.game_manager as gm
+from util.observable import Observable
 
 
-class Player(object):
+class Player(Observable):
     """ A player in the Battleships game.
+    === Private Attributes ===
+        _name:
+            The name of the player
+        _sunken_ships:
+            The number of enemy ships that this player has sunk
+        _num_of_ships:
+            The total num of players ship
+        _ships:
+            A List of this players ships
+        _remaining_ships:
+            The number of ships this player has that are not sunk
+        _guesses:
+            A list that records the locations of any shots made
+            (regardless of hit or miss)
     """
+
     _name: str
-    _sunken_ships: int  # Number of enemy ships that this player has sunk
-    _num_of_ships: int  # Total num of players ship
+    _sunken_ships: int
+    _num_of_ships: int
     _ships: List[Ship]
     _remaining_ships: int
+    _guesses: List[Tuple[int, int, bool]]
 
     def __init__(self, name: str):
+        Observable.__init__(self)
+
         self._name = name
-        self._ships = []  # all ships belonging to player
+        self._ships = [Ship([(0, 0), (1, 1)])]
         self._remaining_ships = len(self._ships)
         self._sunken_ships = 0
         self.num_of_ships = 6
+        self._guesses = []
 
-    def attack(self):
-        """Abstract method: to be implemented in playerComputer and playerHuman
+    def square_clicked(self, coordinate: Tuple[int, int]):
+        """Called when a square on the grid is clicked. To be implemented in
+        sub classes.
         """
         pass
 
-    def is_valid_coordinate(self, row, col):
-        """Returns true if <row> and <col> on the grid.
+    def guess(self, coordinate: Tuple[int, int]):
+        """Called by a player's sqaure_clicked. Takes a shot at the given
+        <coordinate>, resulting in either a Hit, Miss, or Sinking an enemy
+        ship.
         """
-        # TODO: implement the grid
+        for guess in self._guesses:
+            if(guess[:2] == coordinate):
+                return
 
-        board_len = 8  # board_len = grid.get_len, once implemented
-        grid = [[None]]
-        if (0 <= row <= board_len) and \
-           (0 <= col <= board_len) and \
-           (grid[row][col] is None):
-            return True
+        result = gm.GameManager.instance.guess(self, coordinate)
+        if(result == 0):
+            print(self._name, "Miss")
+            self._guesses.append((coordinate[0], coordinate[1], False))
+        elif(result == 1):
+            print(self._name, "Hit")
+            self._guesses.append((coordinate[0], coordinate[1], True))
+        elif(result == 2):
+            print(self._name, "SUNK")
+            self._guesses.append((coordinate[0], coordinate[1], True))
+            self._sunken_ships += 1
         else:
-            return False
+            return
+
+        self.notify_observers()
+
+    def get_guesses(self) -> List[Tuple[int, int, bool]]:
+        """Returns the List of this player's guesses.
+        """
+        return self._guesses
+
+    def get_ships(self) -> List[Ship]:
+        """Returns the List of this player's ships.
+        """
+        return self._ships
+
+    def on_turn_started(self):
+        """Called at the start of a player's turn.
+        To be implemented in sub classes.
+        """
+        pass
 
     def get_name(self) -> str:
-        """Returns the players name
+        """Returns the players name.
         """
         return self._name
 
@@ -48,12 +98,17 @@ class Player(object):
         return self._sunken_ships
 
     def set_num_of_ships(self, num: int):
-        """Changes the number of ships to <num>
+        """Changes the number of ships to <num>.
         """
         self._num_of_ships = num
 
+    def add_num_of_ships(self):
+        """Adds one to the number of ships a player has.
+        """
+        self._num_of_ships += 1
+
     def get_num_of_ships(self) -> int:
-        """Returns the total number of ships that the player has
+        """Returns the total number of ships that the player has.
         """
         return self._num_of_ships
 
@@ -63,12 +118,22 @@ class Player(object):
         self._sunken_ships += 1
 
     def get_ships_left(self) -> int:
-        """Return the number of ships that have not been sunk by enemy
+        """Return the number of ships that have not been sunk by enemy.
         """
         return self._remaining_ships
 
     def decrement_remaining_ships(self):
-        """Update how many remaining ships this player has left.
+        """Decrease how many remaining ships this player has left, by one.
         """
         if self._remaining_ships > 0:
             self._remaining_ships -= 1
+
+    def hit(self, coordinate: Tuple[int, int]) -> int:
+        """Returns whether the a players ship is at <coordinate> and if a shot
+        at <coordinate> would be a hit, miss, or sink a ship.
+        """
+        for ship in self._ships:
+            result = ship.hit(coordinate)
+            if(result != 0):
+                return result
+        return 0
