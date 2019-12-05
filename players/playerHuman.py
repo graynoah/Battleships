90c639f7d8fe7296ce1a60e9680ef10cc3b7a402
Noah Gray <noah.gray@mail.utoronto.ca>
2019-12-04 19:33:39 -0500

    Add Functionality of PlayerHuman Class
    This commit adds a function that allows the player to take a shot when a
    spot on the enemy's grid is clicked.

--- players/playerHuman.py
+++ players/playerHuman.py
@@ -1,25 +1,15 @@
+from typing import Tuple
+
 from players.player import Player
 
 
 class PlayerHuman(Player):
-    """ A Human player in the battleships game
+    """ A Human player in the battleships game.
+    Subclass of Player Class.
     """
 
     def __init__(self, name: str):
         Player.__init__(self, name)
 
-    def attack(self):
-        """Allow the player to input a move to be made
-        """
-        # TODO
-
-        while True:
-            print("Enter the row and col you would like to attack:")
-            row = input("row:")
-            col = input("col:")
-            if self.is_valid_coordinate(row, col):
-                print("You shot at [" + row + ", " + col + "].")
-                break
-            else:
-                print("That is not a valid coordinate")
-
+    def square_clicked(self, coordinate: Tuple[int, int]):
+        Player.guess(self, coordinate)
