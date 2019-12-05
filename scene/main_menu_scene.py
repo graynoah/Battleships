491d8d1fc10222352c1f7f4d786b2e90a7ff2b30
Noah Gray <noah.gray@mail.utoronto.ca>
2019-12-04 19:22:33 -0500

    Merge branch 'master' of https://github.com/graynoah/Battleships

--- scene/main_menu_scene.py
+++ scene/main_menu_scene.py
@@ -9,67 +9,88 @@ from components.slider import Slider
 from components.textbox import Textbox
 from components.vertical_panel import VerticalPanel
 import managers.scene_manager as sm
+from managers.game_manager import GameManager
+from players.playerHuman import PlayerHuman
 
 
 class MainMenuScene(object):
-    def __init__(self, root: Panel):
+    """A menu menu for the game."""
 
+    def __init__(self, root: Panel):
+        """Create a new MainMenuScene, creating the gui components to
+        display.
+        """
         size = sm.SceneManager.instance.get_screen_size()
         title_rect = Rect(0, 0, 500, 70)
         title_rect.center = (size[0] / 2, size[1] / 2 - 200)
 
-        self.options_panel = VerticalPanel(rect=Rect(size[0] / 4, size[1] / 2,
-                                                     size[0] / 2, size[1] / 4),
-                                           expand_height=False,
-                                           parent=root)
+        # A panel for all the options
+        options_panel = VerticalPanel(rect=Rect(size[0] / 4, size[1] / 2,
+                                                size[0] / 2, size[1] / 4),
+                                      expand_height=False,
+                                      parent=root)
 
-        self.title_label = Label(text="BATTLE SHIP!",
-                                 rect=title_rect,
-                                 style=Style(background_color=None,
-                                             border_width=0,
-                                             font=pygame.font.Font(
-                                                 'freesansbold.ttf', 64),
-                                             primary_color=(255, 255, 255)),
-                                 parent=root)
+        # Title label
+        Label(text="BATTLE SHIP!",
+              rect=title_rect,
+              style=Style(background_color=None,
+                          border_width=0,
+                          font=pygame.font.Font(
+                              'freesansbold.ttf', 64),
+                          primary_color=(255, 255, 255)),
+              parent=root)
 
+        # A style for all of the menu options
         button_style = Style(primary_color=(255, 255, 255),
                              background_color=(128, 0, 0),
                              border_width=1,
                              border_color=(0, 0, 0),
                              font=pygame.font.Font('freesansbold.ttf', 32))
 
-        self.pvc_button = Button(rect=Rect(0, 0, 400, 40),
-                                 on_click=self._pvc_clicked,
-                                 text="Player vs Computer",
-                                 style=button_style,
-                                 parent=self.options_panel)
+        # Player vs Computer button
+        Button(rect=Rect(0, 0, 400, 40),
+               on_click=self._pvc_clicked,
+               text="Player vs Computer",
+               style=button_style,
+               parent=options_panel)
 
-        self.pvp_button = Button(rect=Rect(0, 0, 400, 40),
-                                 on_click=self._pvp_clicked,
-                                 text="Player vs Player",
-                                 style=button_style,
-                                 parent=self.options_panel)
+        # Player vs Player button
+        Button(rect=Rect(0, 0, 400, 40),
+               on_click=self._pvp_clicked,
+               text="Player vs Player",
+               style=button_style,
+               parent=options_panel)
 
-        self.settings_button = Button(rect=Rect(0, 0, 400, 40),
-                                      on_click=self._settings_clicked,
-                                      text="Settings",
-                                      style=button_style,
-                                      parent=self.options_panel)
+        # Settings button
+        Button(rect=Rect(0, 0, 400, 40),
+               on_click=self._settings_clicked,
+               text="Settings",
+               style=button_style,
+               parent=options_panel)
 
-        self.quit_button = Button(rect=Rect(0, 0, 400, 40),
-                                  on_click=self._quit_clicked,
-                                  text="Quit",
-                                  style=button_style,
-                                  parent=self.options_panel)
+        # Quit button
+        Button(rect=Rect(0, 0, 400, 40),
+               on_click=self._quit_clicked,
+               text="Quit",
+               style=button_style,
+               parent=options_panel)
 
     def _pvc_clicked(self, button: int):
-        pass
+        """Start a new game of player vs computer."""
+        GameManager.instance.setup_game(
+            PlayerHuman("Player"), PlayerHuman("Computer"))
+        sm.SceneManager.instance.change_scene(2)
 
     def _pvp_clicked(self, button: int):
-        pass
+        """Start a new game of player vs player."""
+        GameManager.instance.setup_game(
+            PlayerHuman("Player1"), PlayerHuman("Player2"))
+        sm.SceneManager.instance.change_scene(2)
 
     def _settings_clicked(self, button: int):
+        """Open the settings menu."""
         sm.SceneManager.instance.change_scene(1)
 
     def _quit_clicked(self, button: int):
+        """Close the game."""
         sm.SceneManager.instance.quit_game()
